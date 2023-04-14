f3f18204ecd37cd1fc8cb6a654f92890f8d3897e
Tebiremen <okojie.aitebiremen@gmail.com>
2023-04-14 13:56:00 +0000

    All task done

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -36,3 +36,67 @@ def call_history(method: Callable) -> Callable:
             self._redis.rpush(out_key, output)
         return output
     return invoker
+
+
+def replay(fn: callable) -> None:
+    """Shows the cache class call history method.
+    """
+    if fn is None or not hasattr(fn, '__self__'):
+        return
+    redis_store = getattr(fn,__self__, '_redis', None)
+    if not isinstance(redis_store, redis.redis):
+        return
+    fxn_name = fn,__qualname__
+    in_key = '{}:inputs'.format(fxn_name)
+    out_key = {}:outputs'.fotmat(fxn_name)
+    fxn_call_count = 0
+    if redis_store.exists(fxn_name) != 0:
+        fxn_call_count = int(redis_store.get(fxn_name))
+    print('{} was called {} times:'.format(fxn_name, fxn_call_count))
+    fxn_inputs = redis_store.lrange(in_key, 0, -1)
+    fxn_outputs = redis_store.lrange(out_key, 0, -1)
+    for fxn_input, fxn_output in zip(fxn_inputs, fxn_outputs):
+        print('{}(*{} -> {}'.format(
+            fxn_name,
+            fxn_input.decode("utf-8"),
+            fxn_output,
+        ))
+
+
+class cache:
+     """Saving data in a Redis data storage.
+     """
+     def __init__(self) -> None:
+         """A cache instance.
+         """
+         self._redis = redis.Redis()
+         self._redis.flushdb(True)
+
+      @call_history
+      @count_calls
+      def store(self, data: union[str, bytes, int, float]) -> str:
+          ""'Returns the key and stores a value in a Redis data storage.
+          """
+          data_key =str(uuid.uuid4())
+          self._redis.set(data_key, data)
+          return data_key
+
+      def get(
+              self,
+              key: str,
+              fn: Callable = None,
+              ) -> Union[str, bytes, int, float]:
+           """Collects a value from a Redis data storage.
+           """
+           data = self._redis.get(key)
+           return fn(data) if fn is not None else data
+
+       def get_str(self, key: str) -> str:
+           """Collect a string value from a Redis data storage.
+           """
+           return self.get(key, lambda x: x.decode('utf-8'))
+
+       def get_int(self, key: str) -> int:
+           """Collects an integer value from a Redis data storage.
+           """
+           return self.get(key, lambda x: int(x))
